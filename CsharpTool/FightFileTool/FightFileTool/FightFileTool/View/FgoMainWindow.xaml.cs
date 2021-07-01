f90e408e40bfdad91ce9249a05a3efe1201cc921
tuo-xy6f <tonylintuo@live.com>
2021-07-01 11:43:37 +0800

    摸鱼2

--- CsharpTool/FightFileTool/FightFileTool/FightFileTool/View/FgoMainWindow.xaml.cs
+++ CsharpTool/FightFileTool/FightFileTool/FightFileTool/View/FgoMainWindow.xaml.cs
@@ -18,11 +18,13 @@ namespace FightFileTool
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class FgoMainWindow : Window
     {
-        public MainWindow()
+        public FgoMainWindow()
         {
             InitializeComponent();
         }
+
+
     }
 }
